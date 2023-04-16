a1573e9591336696af53d6f1c4e7e4bb49d882a8
이승훈 <seunghoon@iseunghun-ui-noteubug.local>
2023-04-17 01:55:58 +0900

    myCounterApp

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
+import { useState } from "react";
+import {
+  useContract,
+  useContractWrite,
+  useContractRead,
+} from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  const contractAddress = "0x3db71a58a48F3Ca96483F5aD15f2d45F88f6f7D8";
+  const { contract } = useContract(contractAddress);
+
+  // get
+  const { data } = useContractRead(contract, "getCounter");
+  const [counter, setCounter] = useState();
+
+  // 비동기함수
+  async function getCounter() {
+    if (!contract) return;
+    setCounter(parseInt(data._hex));
+  }
+
+  // increment
+  const { mutateAsync: incrementCounter } = useContractWrite(
+    contract,
+    "incrementCounter"
+  );
+
+  // decrement
+  const { mutateAsync: decrementCounter } = useContractWrite(
+    contract,
+    "decrementCounter"
+  );
+  return (
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+        </h1>
+
+        <p className="description">
+          Get started by configuring your desired network in{" "}
+          <code className="code">src/index.js</code>, then modify the{" "}
+          <code className="code">src/App.js</code> file!
+        </p>
+
+        <div className="connect">
+          <ConnectWallet
+            dropdownPosition={{ side: "bottom", align: "center" }}
+          />
+        </div>
+
+        <div>{parseInt(data)}</div>
+
+        <br />
+        <Web3Button contractAddress={contractAddress} action={getCounter}>
+          Refresh Counter
+        </Web3Button>
+        <br />
+        <Web3Button contractAddress={contractAddress} action={incrementCounter}>
+          Increment Counter
+        </Web3Button>
+        <br />
+        <Web3Button contractAddress={contractAddress} action={decrementCounter}>
+          Decrement Counter
+        </Web3Button>
+      </main>
+    </div>
+  );
+}
